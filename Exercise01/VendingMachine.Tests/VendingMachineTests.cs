276bd8321edcaccc9730b76edc4394623acf367a
Jan Van Ryswyck <jan.van.ryswyck@principal-it.be>
2024-10-18 20:35:33 +0200

    Added skeleton for exercise 1

---
+++ Exercise01/VendingMachine.Tests/VendingMachineTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace VendingMachine.Tests;
+
+public class VendingMachineTests
+{
+    [Test]
+    public void StartHere()
+    {
+        var vendingMachine = new VendingMachine();
+        // TODO: Implement ...
+    }
+}
