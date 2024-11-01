cf9b25a92f36453a9ec5624039221b3cf4ef64de
Jan Van Ryswyck <jan.van.ryswyck@principal-it.be>
2024-11-01 20:49:02 +0100

    Added combinatorial exercise

--- Exercise03/LegacyCode.Tests/ApprovalTests.cs
+++ Exercise03/LegacyCode.Tests/ApprovalTests.cs
@@ -5,6 +5,7 @@ namespace PrincipalIT.LegacyCode.Tests;
 public class ApprovalTests
 {
     private OutputCatcher _outputCatcher = null!;
+    private VerifySettings _verifySettings;
 
     [SetUp]
     public void SetUp()
@@ -19,6 +20,9 @@ public class ApprovalTests
         
         // _outputCatcher = new OutputCatcher();
         // _outputCatcher.Start();
+        
+        // _verifySettings = new VerifySettings();
+        // _verifySettings.UseDirectory("Approvals");
     }
     
     [TearDown]
@@ -41,6 +45,9 @@ public class ApprovalTests
     //
     // Tip: Check the documentation: https://github.com/VerifyTests/Verify?tab=readme-ov-file#nunit
     //
+    // Tip: Specify the `_verifySettings` to the `Verify` method so that the output files are written to the Approvals
+    // folder.
+    //
     // Tip: Make sure that the test passes after approval. If you run into a date/time related issue, have a look
     // at the `ScrubDateTime` method of the `DateScrubber` class (see Utils folder). For scrubbing date/time strings,
     // you can use `Verify(...).ScrubLinesWithReplace(...)`.
@@ -60,6 +67,8 @@ public class ApprovalTests
     // the `Print` method. A more comprehensive way is to verify all scenarios in one go.
     //
     // Tip: Expand the `ExpenseScenarios` method for adding more scenarios.
+    // Tip: Specify the `_verifySettings` to the `Verify` method so that the output files are written to the Approvals
+    // folder.
     // Tip: For scrubbing date/time strings, you can use `Verify(...).ScrubLinesWithReplace(...)`.
     // Tip: Use a code coverage tool to verify whether all code paths have been executed through the implementation of
     // the `Print` method.
@@ -98,4 +107,40 @@ public class ApprovalTests
             //
         ];
     }
+    
+    // Step 5: A combinatorial approval test
+    //
+    // Let's try a different approach compared to step 4. Another way would be to use a combination of parameters.
+    // In our case we can create combinations of expense types and amounts. These combinations can be verified using
+    // `CombinationApprovals.VerifyAllCombinations`.
+    //
+    // Tip: Although `CombinationApprovals.VerifyAllCombinations` doesn't support data scrubbing, you can still use
+    // `DateScrubber.ScrubDateTime` inside the `DoPrint`method.
+    // Tip: Use a code coverage tool to verify whether all code paths have been executed through the implementation of
+    // the `print` method.
+    [Test]
+    public void PrintExpenseCombinations()
+    {
+        //
+        // TODO: Provide the necessary expense types / amounts to be used to verify combinations.
+        //
+        var expenseTypes = Enumerable.Empty<ExpenseType>();
+        var amounts = Enumerable.Empty<int>();
+        
+        Combination(settings:_verifySettings)
+            .Verify(DoPrint, expenseTypes, amounts)
+            .ScrubLinesWithReplace(DateScrubber.ScrubDateTime);
+    }
+    
+    private string DoPrint(ExpenseType expenseType, int amount) {
+
+        //
+        // TODO: Create an `Expense` instance using the specified parameters, call the `Print` method on the `ExpenseSheet`
+        // class and return the captured output.
+        //
+        return string.Empty;
+    }
+    
+    // Step 6: Take some time to review the tests for step 4 and step 5. What are the differences between these tests?
+    // Which approach is best suited in our case? Why?
 }
